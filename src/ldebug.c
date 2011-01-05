6f360c47b332ac6c9c9ef423f2095d0c2ff97575
chkang <chkang@nextplay.co.kr>
2011-01-05 11:05:44 +0900

    pp: lua-5.1.4 Remove auto string<->number conversion

--- src/ldebug.c
+++ src/ldebug.c
@@ -586,8 +586,7 @@ void luaG_concaterror (lua_State *L, StkId p1, StkId p2) {
 
 
 void luaG_aritherror (lua_State *L, const TValue *p1, const TValue *p2) {
-  TValue temp;
-  if (luaV_tonumber(p1, &temp) == NULL)
+  if (!ttisnumber(p1))
     p2 = p1;  /* first operand is wrong */
   luaG_typeerror(L, p2, "perform arithmetic on");
 }
