229c94bcafbd69be2d714789dc1af23d964d6e58
chkang <chkang@nextplay.co.kr>
2011-02-13 15:27:14 +0900

    lfw support

--- etc/luavs.bat
+++ etc/luavs.bat
@@ -1,21 +1,21 @@
 @rem Script to build Lua under "Visual Studio .NET Command Prompt".
 @rem Do not run from this directory; run it from the toplevel: etc\luavs.bat .
-@rem It creates lua51.dll, lua51.lib, lua.exe, and luac.exe in src.
+@rem It creates lua5.1.dll, lua5.1.lib, lua.exe, and luac.exe in src.
 @rem (contributed by David Manura and Mike Pall)
 
 @setlocal
-@set MYCOMPILE=cl /nologo /MD /O2 /W3 /c /D_CRT_SECURE_NO_DEPRECATE
+@set MYCOMPILE=cl /nologo /MD /O2 /W3 /c /D_CRT_SECURE_NO_DEPRECATE /DLUA_USER_H="""luser.h"""
 @set MYLINK=link /nologo
 @set MYMT=mt /nologo
 
 cd src
 %MYCOMPILE% /DLUA_BUILD_AS_DLL l*.c
 del lua.obj luac.obj
-%MYLINK% /DLL /out:lua51.dll l*.obj
-if exist lua51.dll.manifest^
-  %MYMT% -manifest lua51.dll.manifest -outputresource:lua51.dll;2
+%MYLINK% /DLL /out:lua5.1.dll l*.obj
+if exist lua5.1.dll.manifest^
+  %MYMT% -manifest lua5.1.dll.manifest -outputresource:lua5.1.dll;2
 %MYCOMPILE% /DLUA_BUILD_AS_DLL lua.c
-%MYLINK% /out:lua.exe lua.obj lua51.lib
+%MYLINK% /out:lua.exe lua.obj lua5.1.lib
 if exist lua.exe.manifest^
   %MYMT% -manifest lua.exe.manifest -outputresource:lua.exe
 %MYCOMPILE% l*.c print.c
